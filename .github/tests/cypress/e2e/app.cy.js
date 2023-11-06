cd3e6e0ba2710d1de7c0a6d5d9fa3dd0e1c7cdc6
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-11-07 01:12:44 +0200

    Update app.cy.js

--- .github/tests/cypress/e2e/app.cy.js
+++ .github/tests/cypress/e2e/app.cy.js
@@ -1,8 +1,8 @@
 describe('Demo  Web App Test', () => {
   
   it('Start page successfully loads', () => {
-    cy.visit(Cypress.env('CYPRESS_BASE_URL'));
-    //cy.visit('/')
+    //cy.visit(Cypress.env('CYPRESS_BASE_URL'));
+    cy.visit('http://localhost')
   });
 
   it('Start page shows right content', () => {
