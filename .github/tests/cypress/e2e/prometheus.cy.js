e3cbcde1e3f6e4f42e9343bd09ce49f4ec759c58
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-11-07 13:00:37 +0200

    Reverted prometheus.cy.js

--- .github/tests/cypress/e2e/prometheus.cy.js
+++ .github/tests/cypress/e2e/prometheus.cy.js
@@ -1,16 +1,10 @@
 describe('Prometheus Test', () => {
 
   beforeEach(() => {
-    //const someMetric = 'container_cpu_usage_seconds_total';
-    const someMetric = 'up';
-    cy.visit('127.0.0.1:9090');
-    //cy.visit(Cypress.env('CYPRESS_PROMETHEUS_URL'));
+    const someMetric = 'container_cpu_usage_seconds_total';
+    cy.visit(Cypress.env('PROMETHEUS_URL'));
     cy.get('div.cm-line').type(someMetric);
     cy.get('button.execute-btn').click();
-  });
-
-  it('/graph', () => {
-    cy.url().should('match', /graph/);
   });
 
   it('The container with "webapp" service incuded to monitoring', () => {
