c57406e78301a457d493257879dc985c0b5a6319
Oleksander Tarakanov <Ster1ing@users.noreply.github.com>
2023-11-07 13:05:07 +0200

    Update prometheus.cy.js

--- .github/tests/cypress/e2e/prometheus.cy.js
+++ .github/tests/cypress/e2e/prometheus.cy.js
@@ -1,14 +1,15 @@
 describe('Prometheus Test', () => {
 
   beforeEach(() => {
-    const someMetric = 'container_cpu_usage_seconds_total';
+    //const someMetric = 'container_cpu_usage_seconds_total';
+    const someMetric = 'up';
     cy.visit(Cypress.env('PROMETHEUS_URL'));
     cy.get('div.cm-line').type(someMetric);
     cy.get('button.execute-btn').click();
   });
 
   it('The container with "webapp" service incuded to monitoring', () => {
-    cy.get('span').should('contain', 'webapp');
+    cy.get('span').should('contain', 'webappTest2233');
   });
 
   it('The container with "redis" service incuded to monitoring', () => {
